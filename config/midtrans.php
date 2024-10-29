421dcb4247cc74bdd25021af60b0a319a541d417
LAPTOP-E2U5TP4H\ASUS <gilbranbambimboo@gmail.com>
2024-10-29 08:40:17 +0700

    update

--- config/midtrans.php
+++ config/midtrans.php
@@ -2,9 +2,8 @@
 
     return [
         'server_key' => env('MIDTRANS_SERVER_KEY'),
-        'client_key' => env('MIDTRANS_CLIENT_KEY'),
-        'is_production' => false, // set to true if in production
-        'is_sanitized' => true,
-        'is_3ds' => true,
+        'isProduction' => env('MIDTRANS_IS_PRODUCTION'),
+        'isSanitized' => env('MIDTRANS_IS_SANITIZED'),
+        'is3ds' => env('MIDTRANS_IS_3DS'),
     ];
     
